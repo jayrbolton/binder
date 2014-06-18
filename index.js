f23d536db37d609440cd32cb6dd08e8257423bd8
Jay R Bolton <jayrbolton@gmail.com>
2014-06-18 14:31:19 -0700

    massive simplification. also put on npm

--- index.js
+++ index.js
@@ -1,56 +1,16 @@
-var binder = function() {
-	if (!(this instanceof binder)) return new binder()
-	this.bindings = {}, this.options = {}
-	return this
+function bind(attr, x, y) {
+	if (typeof x === 'function') {
+		var node = document
+		var fn = x
+	} else {
+		var node = x
+		var fn = y
+	}
+	console.log('[' + attr + ']', node.querySelectorAll('*[' + attr + ']'))
+	var nodes = node.querySelectorAll('[' + attr + ']')
+	console.log(nodes)
+	for(var i = 0; i < nodes.length; ++i)
+		fn(nodes[i], nodes[i].getAttribute(attr))
 }
 
-module.exports = binder
-
-binder.prototype.bind = function(attr_name, fn, options) {
-	this.bindings[attr_name] = fn
-	if (options) this.options[attr_name] = options
-	return this
-}
-
-// find and call bindings
-binder.prototype.render = function(el) {
-	var self = this
-	each_node(el, function(node) {
-		var eagers = [], lazies = [], scoped = false
-		if (!node.attributes) return []
-		each(node.attributes, function(attr) {
-			var binding = self.bindings[attr.name]
-			if (binding) {
-				var bind_obj = {f:binding, n:node, a:attr}
-				var opts = self.options[attr.name]
-				;(opts && opts.eager ? eagers : lazies).push(bind_obj)
-				scoped = opts && opts.scoped
-			}
-		})
-		apply_attrs(eagers, lazies)
-		return scoped ? [] : node.childNodes
-	})
-	return this
-}
-
-// iteratively recurse through the dom with a stack and apply a function to every
-// node. the return value of the function should supply (or not) the childNodes.
-var each_node = function(el, fn) {
-	var stack = [el]
-	while (stack.length) push_nodes(stack, fn(stack.pop()))
-}
-
-// apply eager bindings first, then the others
-var apply_attrs = function(eagers, lazies) {
-	var apply_binding = function(b) {b.f(b.n, b.a.value, b.a.name)}
-	each(eagers, apply_binding)
-	each(lazies, apply_binding)
-}
-
-// arr.push.apply(arr, node_list) does not work in ie6
-var push_nodes = function(arr, node_list) {
-	for (var i = 0; i < node_list.length; ++i) arr.push(node_list[i])
-}
-
-// for loops blerg
-var each = function(xs, fn) { for(var i = 0; i < xs.length; ++i) fn(xs[i]) }
+module.exports = bind
--- test/index.js
+++ test/index.js
@@ -1,39 +1,15 @@
 var assert = require('assert')
 var domify = require('domify')
-var binder = require('binder')
+var bind = require('../')
 
 describe('binder', function() {
 
-	it ('binds a function to an attribute', function() {
-		var called = 0
-		var el = domify('<div><div data-bind></div></div>')
-		binder()
-			.bind('data-bind', function() { ++called })
-			.render(el)
-		assert.equal(called, 1, 'should be called')
-	})
-
-	it ('doesnt render inside scoped bindings', function() {
-		var called = 0
-		var el = domify('<div bind><p scope></p></div>')
-		var inc = function() { ++called }
-		binder()
-			.bind('bind', inc, {scoped: true})
-			.bind('scope', inc)
-			.render(el)
-		assert.equal(called, 1)
-	})
-
-	it ('renders eager bindings first', function() {
-		var val = 0
-		var el = domify('<div lazy eager></div>')
-		var inst = new binder()
-		inst.bind('eager', function() {val += 2}, {eager: true})
-		inst.bind('lazy', function() {val *= 2})
-		inst.render(el)
-		// if lazy is called first, val will be 2
-		// if eager is called first, val will be 4
-		assert.equal(val, 4)
+	it('binds a function to an attribute within a node', function() {
+		var el = domify("<div><div bind></div></div>")
+		bind('bind', el, function(node, val) {
+			node.innerHTML = 'hi'
+		})
+		assert.equal(el.firstChild.innerHTML, 'hi')
 	})
 
 })
