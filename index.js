15e10997b8baffbb23759404f07dccd5c036d441
Jay R Bolton <jayrbolton@gmail.com>
2016-01-27 11:04:17 -0800

    Clean up syntax and add a comment about the params

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-function bind(attr, x, y) {
-	if (typeof x === 'function') {
-		var node = document
-		var fn = x
-	} else {
-		var node = x
-		var fn = y
-	}
-	console.log('[' + attr + ']', node.querySelectorAll('*[' + attr + ']'))
-	var nodes = node.querySelectorAll('[' + attr + ']')
-	console.log(nodes)
-	for(var i = 0; i < nodes.length; ++i)
-		fn(nodes[i], nodes[i].getAttribute(attr))
+// Can do two formats:
+// bind('attr-name', myFunction)               -> binds to all attrs in document
+// bind('attr-name', parentNode, myFunction)   -> binds to all attrs below parentNode
+function bind(attr) {
+  if (typeof arguments[1] === 'function') {
+    var node = document
+    var fn = arguments[1]
+  } else {
+    var node = arguments[1]
+    var fn = arguments[2]
+  }
+  var nodes = node.querySelectorAll('[' + attr + ']')
+  for(var i = 0; i < nodes.length; ++i) {
+    fn(nodes[i], nodes[i].getAttribute(attr))
+  }
 }
 
 module.exports = bind
